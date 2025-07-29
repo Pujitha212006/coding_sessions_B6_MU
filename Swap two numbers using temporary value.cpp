8479d978a5d12c54589b2207fb64a56764950276
Pujitha212006 <potluripujitha21@gmail.com>
2025-07-29 18:00:41 +0530

    Swap two numbers using temporary value.cpp

--- Swap two numbers using temporary value.cpp
+++ Swap two numbers using temporary value.cpp
@@ -2,16 +2,16 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int a,b,temp;
-    cout<<"enter two numbers";
-    cin>>a>>b;
-    cout<<"before swap";
-    cout<<"\na="<<a<<"b="<<b;
-    swap(a,b);
-    temp=a;
-    a=b;
-    b=temp;
-    cout<<"\nafter swap";
-    cout<<"\na="<<a<<"b="<<b;
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    cout << "Before swap";
+    cout << "\na = " << a << " b = " << b;
+    int temp = a;
+    a = b;
+    b = temp;
+    cout << "\nAfter swap";
+    cout << "\na = " << a << " b = " << b;
+
     return 0;
 }
